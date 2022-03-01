c4da4f2c45cd8d5a6212c468a873afcc572e0aea
Anna Lubarskaya <ovod125@bk.ru>
2022-03-01 21:42:35 +0300

    вносит изменения с учетом замечаний наставника-1

--- js/main.js
+++ js/main.js
@@ -4,58 +4,30 @@
 function getRandomeNumber(from,to){
 //использую подход early return(сначала ошибки)
 //определяю ситуацию с ошибкой в аргументах (отрицательный диапазон)
-// исходя из формулы получается что отрицательное значение может получиться только если to будет отриц-м
-  try{
-    if (to < 0){
-      throw new RangeError('Параметр to должены быть больше 0');
-    }
-//определяю ситуацию с ошибкой в аргументах (from больше to, в этом случае полученое число будет отрицательным)
-    if (from > to){
-      throw new RangeError('Параметр from должены быть меньше параметра to');
-    }
-  }
-  catch(error) {
-    return 'Введены некорректные значения аргументов. Параметр to должены быть больше 0. Параметр from должены быть меньше параметра to';}
 
-  return randomeNumber = Math.round(Math.random() * (to - from)) +   from;
-}
-getRandomeNumber(100,150);
-
-/*  Остается непонятным можно ли применять throw без try..cathc, например вот так:
-
-function getRandomeNumber(from,to){
   if (to < 0){
     throw new RangeError('Параметр to должены быть больше 0');
   }
+  //определяю ситуацию с ошибкой в аргументах (from больше to, в этом случае полученое число будет отрицательным)
   if (from > to){
     throw new RangeError('Параметр from должены быть меньше параметра to');
   }
-  return randomeNumber = Math.round(Math.random() * (to - from)) +   from;
+  //проверка если from меньше 0
+  if (from < 0) {
+    throw new RangeError('Параметр from должены быть больше 0');
+  }
+  return Math.round(Math.random() * (to - from)) +   from;
 }
-  getRandomeNumber(200,18);
-*/
-
-
+getRandomeNumber(100,150);
 
 /*Функция для проверки максимальной длины строки(использую тернарный оператор) */
 
 function checkMaxLength (facktLengthString, maxLengthString = 140){
 
-  return facktLengthString.length <= maxLengthString? true: false;
+  return facktLengthString.length <= maxLengthString;
+  /*здесь ESLint ругался что слишком сложно и можно проще чем
+  facktLengthString.length <= maxLengthString? true: false;, хотел еще более короткой записи
+  */
 }
 checkMaxLength('');
 
-// если проверить еще на наличие комментария
-
-function checkMaxLength1 (facktLengthString, maxLengthString = 15){
-  try{
-    if(!facktLengthString.length){
-      throw new RangeError('введите комментарий');
-    }
-  } catch (error) {
-  //alert("введите ваш комментарий");
-    return false};
-
-  return facktLengthString.length <= maxLengthString? true: false;
-}
-checkMaxLength1('доkjgljgljdkgljdfgkljdklgjdklfgjdkljgdklfjglkfjgldkfjkj');
